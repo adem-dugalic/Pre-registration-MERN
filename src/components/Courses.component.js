ba8d4d327c258957271d57968957e7998581757a
skeri123 <dugalice@gmail.com>
2020-05-25 00:17:44 +0200

    More component changes

--- src/components/Courses.component.js
+++ src/components/Courses.component.js
@@ -2,71 +2,9 @@ import React, { Component } from "react";
 import "../css/style.css";
 import Cookie from "js-cookie";
 
-/*const tableStyle = {
-    'paddingLeft': '50px',
-    'paddingRight': '50px'
-};
-
-const buttonStyle = {
-    'padding': '20px'
-};*/
-
 export default class Courses extends Component {
   constructor(props) {
     super(props);
-
-    this.updateTable = this.updateTable.bind(this);
-    this.setPage = this.setPage.bind(this);
-
-    this.state = {
-      data: [],
-      page: 1,
-      nbOfPage: -1,
-      buttonsGroup: [],
-    };
-  }
-
-  //Right before anything load the page this is called
-  async componentDidMount() {
-    await this.updateTable();
-  }
-
-  async updateTable() {
-    const response = await fetch(
-      "http://localhost:5000/courses?page=" +
-        this.state.page +
-        "&token=" +
-        Cookie.get("token") +
-        "&userId=" +
-        Cookie.get("userId")
-    );
-    const res = await response.json();
-
-    console.log(res);
-    let nbOfPage = res.nbOfPage;
-
-    let btns = [];
-    let start;
-    if (this.state.page <= 1) start = 1;
-    else if (nbOfPage > 0 && this.state.page >= nbOfPage) start = nbOfPage - 2;
-    else start = this.state.page - 1;
-
-    for (let i = start; i < start + 3; i++) {
-      btns.push(i);
-    }
-
-    this.setState({
-      buttonsGroup: btns,
-      data: res.array,
-      nbOfPage: nbOfPage,
-    });
-  }
-
-  setPage(page) {
-    //TODO: remove the ugly animation of the button when you click on it
-    console.log("Select page: " + page);
-    this.state.page = page; //Manual change because we don't care and are thugs
-    this.updateTable();
   }
 
   render() {
