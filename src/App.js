f6293a8a6113f12691d8005499e6395a6946c49f
Tarik Muharem <tare98muharem@gmail.com>
2020-05-25 14:38:08 +0200

    App updated - working but not perfect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./css/style.css";
 
-import Navigation from "./components/navbar.component";
 import Login from "./components/Login.component";
 import SignUp from "./components/SignUp.component";
 import Courses from "./components/Courses.component";
@@ -10,26 +9,17 @@ import UserCourses from "./components/UserCourses.component";
 import Home from "./components/Home.component";
 import Schedule from "./components/Schedule.component";
 
-const containerStyle = {
-  maxWidth: "100%",
-  paddingLeft: "0px",
-  paddingRight: "0px",
-};
-
 function App() {
   return (
     <Router>
       <div className="outerWrapper">
-        <div className="wrapper">
-          <Navigation />
-          <Route path="/Home" component={Home} />
-          <Route path="/Courses" component={Courses} />
-          <Route path="/UserCourses" component={UserCourses} />
-          <Route path="/Schedule" component={Schedule} />
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/Home" component={Home}/>
+        <Route exact path="/Schedule" component={Schedule}/>
+        <Route exact path="/Signup" component={SignUp} />
+        <Route exact path="/Courses" component={Courses} />
+        <Route exact path="/UserCourses" component={UserCourses} />
         </div>
-        <Route path="/Login" component={Login} />
-        <Route path="/Signup" component={SignUp} />
-      </div>
     </Router>
   );
 }
