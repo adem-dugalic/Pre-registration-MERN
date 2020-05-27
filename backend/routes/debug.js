fd9bbf1f366a98b20f23c341afc74a1482969c32
Tarik Muharem <tare98muharem@gmail.com>
2020-05-27 19:00:35 +0200

    updated debug routes

--- backend/routes/debug.js
+++ backend/routes/debug.js
@@ -6,6 +6,46 @@ const UserSession = require('../models/UserSession.model');
 const Course = require('../models/Course.model');
 
 
+router.route('/userImport').post((req, res) => {
+
+    const user = new User({
+        userID: req.body.userID,
+        name: req.body.name,
+        surname: req.body.surname,
+        faculty: req.body.faculty,
+        program: req.body.program,
+        semester: req.body.semester,
+        email: req.body.email,
+    });
+
+    user.save().then(
+        () => {
+            res.status(201).json({
+                message: 'User added successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(500).json({
+                error: error
+            });
+        }
+    );
+});
+
+router.route('/finishedCoursesImport').post((req,res) => {
+    const userCourses = new UserCourses ({
+        userId:req.body.userId,
+        finishedCourses: body.req.finishedCourses,
+    })
+})
+
+router.route('/getUserCourses').get((req,res) => {
+    UserCourses.find()
+    .then(courses => res.json(courses))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
 router.route('/users').get((req,res)=> {
     User.find()
         .then(users => res.json(users))
@@ -24,6 +64,15 @@ router.route('/courses').get((req,res)=> {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/delete').delete((req, res) => {
+    User.findOneAndDelete({name:req.body.name})
+    .then(() => {
+        res.status(200).json("Done!");
+    })
+    .catch((err) => {
+        res.status(400).json('Error: ' + err);
+    })
+});
 
 
 
