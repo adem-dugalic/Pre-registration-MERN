fc10d105343c00cd63aa3663d3bf594e8617ae65
skeri123 <dugalice@gmail.com>
2020-05-27 17:50:10 +0200

    Fixed few issues

--- src/components/Courses.component.js
+++ src/components/Courses.component.js
@@ -17,8 +17,8 @@ export default class Courses extends Component {
     this.getCurrentUserCourse = this.getCurrentUserCourse.bind(this);
 
     this.state = {
-      default:[], //We save the course to avoid to requery them each time.
-      userCourse:[],
+      default: [], //We save the course to avoid to requery them each time.
+      userCourse: [],
       data: [],
       page: 1,
       courseId: [],
@@ -27,7 +27,7 @@ export default class Courses extends Component {
       courseID: "",
 
       //search
-      search:"",
+      search: "",
     };
   }
 
@@ -37,16 +37,20 @@ export default class Courses extends Component {
     await this.updateTable();
   }
 
-
-  getCurrentUserCourse()
-  {
-    axios.get("http://localhost:5000/users/userCourse?token=" + Cookie.get("token") + "&userId=" +  Cookie.get("userId"))
-        .then((res) => {
-          this.setState({
-            userCourse: res.data[0].courses,
-          });
-        })
-        .catch((err) => alert("Error: " + err));
+  getCurrentUserCourse() {
+    axios
+      .get(
+        "http://localhost:5000/users/userCourse?token=" +
+          Cookie.get("token") +
+          "&userId=" +
+          Cookie.get("userId")
+      )
+      .then((res) => {
+        this.setState({
+          userCourse: res.data[0].courses,
+        });
+      })
+      .catch((err) => alert("Error: " + err));
   }
 
   async updateTable() {
@@ -70,52 +74,55 @@ export default class Courses extends Component {
     this.setState({ course: event.target.value });
   }; */
 
-  onAddItem(course) {
+  async onAddItem(course) {
     console.log("onAddItem stuff");
     console.log(course);
     axios
-        .post(
-            "http://localhost:5000/users/addCourse?token=" +
-            Cookie.get("token") +
-            "&userId=" +
-            Cookie.get("userId"),
-            {courseId:course}
-        )
-        .then((res) => {
-          console.log("Success");
-        })
-        .catch((err) => alert("Error: " + err));
+      .post(
+        "http://localhost:5000/users/addCourse?token=" +
+          Cookie.get("token") +
+          "&userId=" +
+          Cookie.get("userId"),
+        { courseId: course }
+      )
+      .then((res) => {
+        console.log("Success");
+      })
+      .catch((err) => alert("Error: " + err));
   }
-
 
   // Search impelmentation
   onSearch(e) {
     e.preventDefault();
     this.setState({
-      search:e.target.value
+      search: e.target.value,
     });
     console.log(e.target.value);
 
-    if(e.target.value === 0)
-    {
+    if (e.target.value === 0) {
       this.setState({
-        data:this.state.default,
+        data: this.state.default,
       });
       return;
-    }
-    else if(e.target.value.length<3)
-      return;
+    } else if (e.target.value.length < 3) return;
 
-    axios.get('http://localhost:5000/courses/search?value=' + e.target.value + '&token=' + Cookie.get('token') + "&userId="+Cookie.get('userId'))
-        .then(res => {
-          console.log(res.data);
+    axios
+      .get(
+        "http://localhost:5000/courses/search?value=" +
+          e.target.value +
+          "&token=" +
+          Cookie.get("token") +
+          "&userId=" +
+          Cookie.get("userId")
+      )
+      .then((res) => {
+        console.log(res.data);
 
-          this.setState({data:res.data});
-
-        })
-        .catch((err) => {
-          alert('Error: ' + err);
-        });
+        this.setState({ data: res.data });
+      })
+      .catch((err) => {
+        alert("Error: " + err);
+      });
   }
 
   /*  changeLooks(e) {
@@ -134,7 +141,13 @@ export default class Courses extends Component {
         <div className="upperNav">
           <div className="search">
             <form className="searchForm">
-              <input type="search" name="search" placeholder="Search" onChange={this.onSearch} value={this.state.search} />
+              <input
+                type="search"
+                name="search"
+                placeholder="Search"
+                onChange={this.onSearch}
+                value={this.state.search}
+              />
             </form>
             <button className="magnifier">
               <img src={Magnifier} className="manifierImg" />
@@ -155,46 +168,53 @@ export default class Courses extends Component {
           </div>
         </div>
         <div className="allCourses" id="allCourses">
-            <table className="courses">
-              <tbody>
-                <tr className="info">
-                  <td>Course Code</td>
-                  <td>Course Name</td>
-                  <td>Professor</td>
-                  <td>Faculty</td>
-                  <td>Prerequisites</td>
-                  <td>Option</td>
-                </tr>
-                {this.state.data.map((item, i) => {
-                  return (
-                    <tr key={i} style={this.state.userCourse.includes(item._id)>0?{background:"red"}:{background:"black"}}>
-                      <td className="title">{item.course_id}</td>
-                      <td>
-                        <a href={"https://ecampus.ius.edu.ba/" + item.Url}>
-                          {item.course_name}
-                        </a>
-                      </td>
-                      <td>{item.Lecturer}</td>
-                      <td>{item.AcademicUnit}</td>
-                      <td>
-                        {item.prerequisite.map((item) => {
-                          return item + " ";
-                        })}
-                      </td>
-                      <td>
+          <table className="courses">
+            <tbody>
+              <tr className="info">
+                <td>Course Code</td>
+                <td>Course Name</td>
+                <td>Professor</td>
+                <td>Faculty</td>
+                <td>Prerequisites</td>
+                <td>Option</td>
+              </tr>
+              {this.state.data.map((item, i) => {
+                return (
+                  <tr
+                    key={i}
+                    style={
+                      this.state.userCourse.includes(item._id) > 0
+                        ? { display: "none" }
+                        : { background: "#353131" }
+                    }
+                  >
+                    <td className="title">{item.course_id}</td>
+                    <td>
+                      <a href={"https://ecampus.ius.edu.ba/" + item.Url}>
+                        {item.course_name}
+                      </a>
+                    </td>
+                    <td>{item.Lecturer}</td>
+                    <td>{item.AcademicUnit}</td>
+                    <td>
+                      {item.prerequisite.map((item) => {
+                        return item + " ";
+                      })}
+                    </td>
+                    <td>
+                      <div className="button">
                         <div className="button">
-                          <div className="button">
-                            <button onClick={() => this.onAddItem(item._id)}>
-                              Add Course
-                            </button>
-                          </div>
+                          <button onClick={() => this.onAddItem(item._id)}>
+                            Add Course
+                          </button>
                         </div>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
     );
